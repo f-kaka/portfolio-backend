8dd77055b7b1696099e45ef038b6736be1e6bc8f
f-kaka <kakangafrenki28@gmail.com>
2026-06-03 14:51:18 +0300

    fix openai crash on startup

--- server.js
+++ server.js
@@ -157,8 +157,7 @@ const db = mysql.createConnection({
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  connectTimeout: 60000
+  database: process.env.DB_NAME
 });
 
 db.connect((err) => {
